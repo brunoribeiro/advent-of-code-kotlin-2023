24947b920f17539febd4c2075773eeb5706be052
bruno.ribeiro <bruno.ribeiro@dynamicyield.com>
2023-12-02 22:45:11 +0000

    day02 - update

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -13,27 +13,26 @@ fun main() {
             "blue" to 14
         )
 
-        val sum = input.map { toRound(it) }.filter {
-            it.all { round ->
-                (round.cubes.all {
+        return input.map {
+            toRound(it)
+        }.filter {
+            it.sets.all {
+                (it.all {
                     (checkMap[it.key] ?: 0) >= it.value
                 })
             }
-        }.flatMap {
-            it.groupBy { it.id }.keys.distinct()
-        }.sum()
-
-
-        return sum
+        }.sumOf { it.id }
 
     }
 
     fun part2(input: List<String>): Int {
 
-        return input.map { toRound(it) }.map {
+        return input.map {
+            toRound(it)
+        }.sumOf {
             val tmpMap = mutableMapOf<String, Int>()
-            it.forEach {
-                it.cubes.entries.forEach {
+            it.sets.forEach {
+                it.entries.forEach {
                     tmpMap.compute(it.key) { _, v ->
                         if (it.value > (v ?: 0))
                             it.value
@@ -46,7 +45,8 @@ fun main() {
             tmpMap.values.reduce { acc, i ->
                 acc * i
             }
-        }.sum()
+
+        }
     }
 
     check(part1(readInput("day02/Day02_test")) == 8)
@@ -57,30 +57,25 @@ fun main() {
     part2(input).println()
 }
 
-fun toRound(play: String): List<MappedRound> {
-    val parts = play.split(":").map { it.trim() }
-    val id = parts.first().replace("Game ", "").toInt()
-    val sets = parts.last().split(";")
-    return sets.mapIndexed { i, str ->
-        Round(id, i + 1,
-            str.split(",").map { it.trim() }
-                .map { it.split(" ").let { CubeInfo(it.first().toInt(), it.last()) } }
-        )
-    }.map {
-        MappedRound(
-            id,
-            it.set,
-            joinCubes(it.cubes).groupBy { it.color }.map { it.key to it.value.sumOf { it.amount } }.toMap()
-        )
-    }
+
+fun toRound(str: String): Round {
+    return str.split(":").map { it.trim() }
+        .map { it.replace("Game", "").trim() }
+        .let {
+            Round(
+                it.first().toInt(),
+                toSets(it.last())
+            )
+        }
 }
 
-fun joinCubes(cubes: List<CubeInfo>): List<CubeInfo> {
-    return cubes.groupBy { it.color }
-        .map { it.key to it.value.sumOf { it.amount } }
-        .map { CubeInfo(it.second, it.first) }
+fun toSets(str: String): List<Map<String, Int>> {
+    return str.split(";")
+        .map { it.trim() }
+        .map {
+            it.split(",").map { it.trim() }
+                .associate { it.split(" ").let { it.last() to it.first().toInt() } }
+        }
 }
 
-data class Round(val id: Int, val set: Int, val cubes: List<CubeInfo>)
-data class MappedRound(val id: Int, val set: Int, val cubes: Map<String, Int>)
-data class CubeInfo(val amount: Int, val color: String)
+data class Round(val id: Int, val sets: List<Map<String, Int>>)
